54bcdd4916a74cf3be8885b46f9485e263dee033
Ruba <rabrobaaa@gmail.com>
2025-12-08 22:19:41 +0200

    main window UI

--- app/src/main/java/com/todo/just_do_it/MainActivity.kt
+++ app/src/main/java/com/todo/just_do_it/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.todo.just_do_it
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,32 +8,31 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import com.todo.just_do_it.model.TodoView
 import com.todo.just_do_it.screen.TodoList
+import com.todo.just_do_it.ui.theme.JustDoItTheme // <--- IMPORT THIS!
 
 class MainActivity : ComponentActivity() {
+
     private val viewModel: TodoView by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val todos by viewModel.todos.collectAsState()
+            // --- CHANGE IS HERE ---
+            // We wrap everything inside our custom Theme
+            JustDoItTheme {
 
-            TodoList(
-                todos = todos,
-                onAddClick = { title, desc -> viewModel.addTodo(title, desc) },
-                onDoneClick = { todo -> viewModel.toggleDone(todo) },
-                onDeleteClick = { todo -> viewModel.deleteTodo(todo) }
-            )
+                // Collect the data just like before
+                val todos by viewModel.todos.collectAsState()
+
+                // Show the screen
+                TodoList(
+                    todos = todos,
+                    onAddClick = { title, desc -> viewModel.addTodo(title, desc) },
+                    onDoneClick = { todo -> viewModel.toggleDone(todo) },
+                    onDeleteClick = { todo -> viewModel.deleteTodo(todo) }
+                )
+            }
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
